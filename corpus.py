967ba85e584875e1bffa7e95f90677f03434b9ca
Not A Hero <krsnvijay@gmail.com>
2020-12-12 22:02:30 -0800

    Add novel corpusm, with text cleanup

--- corpus.py
+++ corpus.py
@@ -2,19 +2,33 @@ import nltk
 
 try:
     nltk.data.find('words')
+    nltk.data.find('punkt')
+    nltk.data.find('stopwords')
 except:
     nltk.download('words')
+    nltk.download('punkt')
+    nltk.download('stopwords')
 
 from nltk.corpus import words
 from random import sample, randint
 
 
-def corpus_word_list(n):
-    return sample(words.words(), n)
+def corpus_word_list(corpus_words, n):
+    if len(corpus_words) < n:
+        n = len(corpus_words)
+    if corpus_words is None:
+        corpus_words = words.words()
+    return sample(corpus_words, n)
 
+def novel_random_text_patterns(word_list,n):
+    if len(word_list) < n:
+        n = len(word_list)
+    from_corpus_number = randint(n // 2, n)
+    from_corpus_list = set(corpus_word_list(word_list, from_corpus_number))
+    return list(from_corpus_list)
 
 def randomized_text_patterns(word_list, n):
     from_corpus_number = randint(n // 2, n)
-    from_corpus_list = set(sample(word_list, from_corpus_number))
-    another_random_corpus_list = set(sample(words.words(), n - from_corpus_number))
+    from_corpus_list = set(corpus_word_list(word_list, from_corpus_number))
+    another_random_corpus_list = set(corpus_word_list(words.words(), n - from_corpus_number))
     return list(from_corpus_list.union(another_random_corpus_list))
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import string
+
 from aho_corasick import test_aho_corasick
 from commentz_walter import test_commentz_walter
-from corpus import corpus_word_list, randomized_text_patterns
+from corpus import corpus_word_list, randomized_text_patterns, novel_random_text_patterns
 from rabin_karp import test_rabin_karp
 from synonyms import get_all_patterns
 import matplotlib.pyplot as plt
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import words
+from nltk.corpus import stopwords
 
 METRICS = {
     'cw': [],
@@ -18,14 +24,33 @@ def run_algorithms(n=100, m=5, random=False):
     global METRICS
     if random:
         print(f"Constructing a random corpus of text with {n} words...")
-        search_word_list = corpus_word_list(n)
+        search_word_list = corpus_word_list(words.words(), n)
         search_str = ' '.join(search_word_list)
+        patterns = randomized_text_patterns(search_word_list, m)
+
     else:
         print(f"Retrieving a corpus of text from a novel having {n} words...")
-        search_word_list = corpus_word_list(n) # retrieve from novel
+        file = open("pride-and-prejudice.txt", 'rt', encoding="utf8")
+        novel_text = file.read()
+        file.close()
+        # convert text to tokens
+        tokens = word_tokenize(novel_text)
+        # convert tokens to lowercase
+        tokens = [w.lower() for w in tokens]
+        # remove punctuations for words
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        # only include words that have alphabets
+        novel_words = [word for word in stripped if word.isalpha()]
+        # remove stop words
+        stop_words = set(stopwords.words('english'))
+        novel_words = [w for w in novel_words if not w in stop_words]
+        # take n sample of words from corpus word list
+        search_word_list = corpus_word_list(novel_words, n)  # retrieve from novel
         search_str = ' '.join(search_word_list)
+        # get m sample of words to use as patterns
+        patterns = novel_random_text_patterns(search_word_list, m)
 
-    patterns = randomized_text_patterns(search_word_list, m)
     before_syn_num = len(patterns)
     print("Number of patterns before getting synonyms: ", before_syn_num)
     print(patterns)
@@ -56,8 +81,8 @@ def plot_metrics(random_label='Random words'):
     plt.xlim(0, INSTANCE_SIZES[-1])
     y_limit = int(max(max(METRICS['cw']), max(METRICS['ac']), max(METRICS['rk'])))
     plt.ylim(0, y_limit)
-    plt.xticks(range(0, INSTANCE_SIZES[-1]+2000, 1000))
-    plt.yticks(range(0, y_limit+20, 10))
+    plt.xticks(range(0, INSTANCE_SIZES[-1] + 2000, 1000))
+    plt.yticks(range(0, y_limit + 20, 10))
     plt.title(f"({random_label}) Running times of Commentz-Walter, Aho-Corasick and Rabin-Karp")
     plt.xlabel('Corpus size (in number of words)')
     plt.ylabel('Time (in milliseconds)')
